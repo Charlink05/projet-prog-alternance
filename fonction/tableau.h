ed5a9dd9456d100ee233ecfc32807e27cc46fdab
Jean-dlf <Jean.delaforge22@gmail.com>
2024-11-19 00:37:03 +0100

    Delete fonction directory

--- fonction/tableau.h
+++
@@ -1,8 +0,0 @@
-#ifndef TABLEAU_H
-#define TABLEAU_H
-
-void aff_tableau(tableau ta);
-
-tableau initialisation_tableau(int n, int m);
-
-#endif
--- fonction/vider_buffer.h
+++
@@ -1,6 +0,0 @@
-#ifndef _VIDER_BUFFER_H_
-#define _VIDER_BUFFER_H_
-
-void viderBuffer();
-
-#endif
